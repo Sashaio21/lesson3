e0f111853b50147588a0714725a57f89eded48e0
KOvalSascha <alexandrkowal21@gmail.com>
2024-12-08 17:54:21 +0300

    first commit

---
+++ practical_work.py
@@ -0,0 +1,49 @@
+# number =  int(input("Введите любое числ2525о"))
+
+# if number > 0:
+#     print("Вы ввели положительное число")
+# elif number < 0:
+#     print("Вы ввели отрицательное число")
+# else:
+#     print("Вы ввели ноль")
+
+
+
+# Проверка на не совершеннолетие
+# age = int(input("Сколько вам лет?")) #возраст
+# if age >= 18:
+#     print("Вы совершенно летний")
+#     product = input("пиво  или водку")
+#     if product == "пиво":
+#         print("с вас 2 публя")
+#     elif product == "водка":
+#         print("с вас 5 рублей")
+# else:
+#     print("Вы несовршенно летний")
+
+
+# print("Меню")
+# print("1. Сложение\n2. Отниамние\n 3. Умножение\n 4. Деление\n 5. Выйти из программы")
+# choice = int(input(""))
+
+# a = int(input("Введите число а"))
+# b = int(input("Введите число b"))
+
+# if choice == 1:
+#     c = a + b
+#     print(c)
+# elif choice == 2:
+#     c = a - b
+#     print(c)
+# elif choice == 3:
+#     c = a * b
+#     print(c)
+# elif choice == 4:
+#     c = a / b
+#     print(c)
+
+
+number = int(input("Введите люой число"))
+
+if number > 10 and number < 20:
+    print("Введенное число входит в диапазон от 10 до 20 ")
